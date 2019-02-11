a8068c973b839c21948659bf8f4769625ab4fb55
Leesimok <naru80@godo.co.kr>
2019-02-11 15:24:18 +0900

    Ignored all .log files and added README.md.

---
+++ es6_0207.php
@@ -0,0 +1,10 @@
+
+
+	console.log(i);
+	for (var i = 0; i < 100; i++) {
+	var homevar = "homevar";
+	}
+function home() {
+home();
+var name = 'global var';
+}
---
+++ scope/const_script.js
@@ -0,0 +1,9 @@
+function home(){
+  const homename = [1,2,3,3]
+  homename = ["1", "2"];
+  console.log(homename);
+  //const 기본으로 사용한다.
+  //그런데 변경이 될 수 있는 변수는 let을 사용한다
+  //var는 사용하지 않는다.
+}
+home();
